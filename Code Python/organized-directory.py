9a25541e9cb1df73f8db8ca4a5d0794212b74d3d
Worof <70353304+Worof@users.noreply.github.com>
2023-12-16 23:48:38 +0200

    organized file directory

--- Code Python/organized-directory.py
+++ Code Python/organized-directory.py
@@ -1,35 +1,44 @@
 import os
 import shutil
+import logging  
+import datetime  
+
 
 current_dir = os.path.dirname(os.path.realpath(__file__))
 
-#Organize iamges into image folder
+##File Renaming##
+
+# Rename files based on date:
+def rename_with_date(filename):
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    basename, extension = os.path.splitext(filename)
+    return f"{basename}_{timestamp}{extension}"
+
+#Organize images into image folder
 
 for filename in os.listdir(current_dir):
-      if filename.endswith((".png", ".jpg", ".jpeg", ".gif", ".tif")):
+      if filename.endswith((".png", ".jpg", ".jpeg", ".gif", ".tif", ".bmp")):
         if not os.path.exists("Images"):
             os.mkdir('Images')
-        shutil.copy(filename, 'Images')
-        os.remove(filename)
+        new_filename = rename_with_date(filename)
+        shutil.move(filename, os.path.join("Images", new_filename))
         
-        
-
         
 #organize video files into video folder
 for filename in os.listdir(current_dir):
-      if filename.endswith((".mp4", ".mov", ".wmv", ".flv", ".mkv")):
+      if filename.endswith((".mp4", ".mov", ".wmv", ".flv", ".mkv", ".avi")):
         if not os.path.exists("Videos"):
             os.mkdir('Videos')
-        shutil.copy(filename, 'Videos')
-        os.remove(filename)
+        new_filename = rename_with_date(filename)
+        shutil.move(filename, os.path.join("Videos", new_filename))
         
 #organize docs files into docs folder
 for filename in os.listdir(current_dir):
       if filename.endswith((".pdf", ".doc", ".docx", ".html", ".htm", ".odt", ".xls", ".xlsx", ".ods", ".ppt", ".pptx", ".txt")):
         if not os.path.exists("Documents"):
             os.mkdir('Documents')
-        shutil.copy(filename, 'Documents')
-        os.remove(filename)
+        new_filename = rename_with_date(filename)
+        shutil.move(filename, os.path.join("Documents", new_filename))
 
 
 #organize archives files into archive folder
@@ -37,13 +46,41 @@ for filename in os.listdir(current_dir):
       if filename.endswith((".zip", ".rar")):
         if not os.path.exists("Archive"):
             os.mkdir('Archive')
-        shutil.copy(filename, 'Archive')
-        os.remove(filename)
+        new_filename = rename_with_date(filename)
+        shutil.move(filename, os.path.join("Archive", new_filename))
         
-#organize codes' files into code folder
+#organize python codes' files into code folder
 for filename in os.listdir(current_dir):
       if filename.endswith((".py")):
         if not os.path.exists("Code Python"):
             os.mkdir('Code Python')
-        shutil.copy(filename, 'Code Python')
-        os.remove(filename)
+        new_filename = rename_with_date(filename)
+        shutil.move(filename, os.path.join("Code Python", new_filename))
+
+
+#organize C codes' files into code folder
+for filename in os.listdir(current_dir):
+      if filename.endswith((".c")):
+        if not os.path.exists("Code C"):
+            os.mkdir('Code C')
+        new_filename = rename_with_date(filename)
+        shutil.move(filename, os.path.join("Code C", new_filename))
+        
+        
+#organize music files into music folder
+for filename in os.listdir(current_dir):
+      if filename.endswith((".mp3", ".wav", ".flac", ".ogg")):
+        if not os.path.exists("Music"):
+            os.mkdir('Music')
+        new_filename = rename_with_date(filename)
+        shutil.move(filename, os.path.join("Music", new_filename))
+        
+#organize spreadsheets into music folder
+for filename in os.listdir(current_dir):
+      if filename.endswith((".csv", ".xlsb")):
+        if not os.path.exists("Spreadsheets"):
+            os.mkdir('Spreadsheets')
+        new_filename = rename_with_date(filename)
+        shutil.move(filename, os.path.join("Spreadsheets", new_filename))      
+
+
