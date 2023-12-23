0fb7c37e76979f6b237338b90e778d04fd968b20
Worof <70353304+Worof@users.noreply.github.com>
2023-12-23 13:49:33 +0200

    Converting a PDF book into an audiobook

--- Code Python/text-to-speech.py
+++ Code Python/text-to-speech.py
@@ -2,31 +2,36 @@ import pyttsx3
 import pdfplumber
 from tqdm import tqdm
 
-
 pdfFile = 'great-expectations.pdf'
 
-#Extract the text from the PDF file
-finalText = []
 
+#Initialize text-to-speech engine
+engine = pyttsx3.init()
+#Set the speed of the speech
+engine.setProperty('rate', 150)  
+
+
+#saving the audiobook in a specific file & handeling errors
+def process_text_to_audio(text, file_name):
+    try:
+        engine.save_to_file(text, file_name)
+        engine.runAndWait()
+    except Exception as e:
+        print(f"An error occurred in text-to-speech conversion: {e}")
+#converting the book into audiobook in chunks 
 try:
     with pdfplumber.open(pdfFile) as pdf:
-        pages = len(pdf.pages)
-        for page in tqdm(pdf.pages, desc ='Converting Pages into Audio'):
+        total_pages = len(pdf.pages)
+        print(f"Processing {total_pages} pages...")
+
+        for i, page in enumerate(tqdm(pdf.pages, desc='Converting Pages into Audio')):
             text = page.extract_text()
             if text:
-                finalText.append(text)
+                audio_file = f'great-expectations_page_{i+1}.mp3'
+                process_text_to_audio(text, audio_file)
+        print('Finished Processing Your File...ENJOY!')
+
 except FileNotFoundError:
     print(f"The file {pdfFile} was not found.")
-    exit()
 except Exception as e:
     print(f"An error occurred: {e}")
-    exit()
-
-# Join all extracted text
-finalText = '\n'.join(finalText)
-
-        
-#Convert the text into audio & save it
-engine = pyttsx3.init()
-engine.save_to_file(finalText, 'great-expectations.mp3')
-engine.runAndWait()
