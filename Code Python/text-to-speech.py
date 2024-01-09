568f5b07d7eb9f314222a8389a3d5384fee863ec
Worof <70353304+Worof@users.noreply.github.com>
2024-01-09 21:54:04 +0200

    text-speech conversion

--- Code Python/text-to-speech.py
+++ Code Python/text-to-speech.py
@@ -1,24 +1,51 @@
 import pyttsx3
 import pdfplumber
 from tqdm import tqdm
+from pydub import AudioSegment
+import os
 
-pdfFile = '1984.pdf'
-audio_file = '1984-audiobook.mp3'
+
+pdfFile = input('Enter the file you want to convert (including .pdf extension): ')
+audio_file = input("Enter the desired name for the output audio file (including .mp3 extension): ")
+audio_format = input("Enter the audio format (mp3, wav, etc.): ")
+audio_bitrate = input("Enter the desired bitrate (e.g., 192k, 256k): ")
 
 
 #Initialize text-to-speech engine
 engine = pyttsx3.init()
+
+#listing available voices and allowing the user to choose his preferance
+voices = engine.getProperty('voices')
+for index, voice in enumerate(voices):
+    print(f"{index}: Voice Id: {voice.id}, Language: {voice.languages}, Gender: {voice.gender}")
+
+voice_number = int(input('Select the number of your preferred voice: '))
+selected_voice_id = voices[voice_number].id
+
+#set the chosen voice
+engine.setProperty('voice', selected_voice_id)
 #Set the speed of the speech
-engine.setProperty('rate', 120)  
+engine.setProperty('rate', 150)  
+
+
+    
 
 
 #saving the audiobook in a specific file & handeling errors
-def process_text_to_audio(text):
+def process_text_to_audio(text, audio_file, format='mp3', bitrate='192K'):
+    temp_audio = 'temp_audio.wav'
+    
     try:
         engine.save_to_file(text, audio_file)
         engine.runAndWait()
+        
+        #convert to desired format using pydub
+        audio = AudioSegment.from_wav(temp_audio)
+        audio.export(audio_file, format=format, bitrate=bitrate)
     except Exception as e:
         print(f"An error occurred in text-to-speech conversion: {e}")
+    finally:
+        os.remove(temp_audio)
         
         
 #converting the book into audiobook in chunks 
@@ -36,7 +63,7 @@ try:
                 full_text += text + ''
                 
             
-        process_text_to_audio(full_text)
+        process_text_to_audio(full_text, audio_file, format=audio_format, bitrate=audio_bitrate)
         
         print('Finished Processing Your File...ENJOY!')
 
